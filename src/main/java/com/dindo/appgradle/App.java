129d90ac32cc1bfb64c42bc89552d5ee9d2a552b
Dindo <82750307+dindo-manondo@users.noreply.github.com>
2026-08-17 21:06:12 +0800

    Add App.java

---
+++ src/main/java/com/dindo/appgradle/App.java
@@ -0,0 +1,16 @@
+package com.dindo.appgradle;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from App Gradle!");
+        System.out.println("Build is successful.");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
